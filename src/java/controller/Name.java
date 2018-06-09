17507f5199f6a307e14a02db0bb47066eeab7ab2
dibyendu_pc7 <dibyendu_pc7@dibyendu_pc7-PC>
2018-06-09 17:16:20 +0530

    this is spring mvc application " what is your name" for naives like me

---
+++ src/java/controller/Name.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author dibyendu_pc7
+ */
+public class Name {
+private String Value;
+
+    public String getValue() {
+        return Value;
+    }
+
+    public void setValue(String Value) {
+        this.Value = Value;
+    }
+}
---
+++ web/WEB-INF/helloView.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : helloView
+    Created on : 9 Jun, 2018, 3:26:58 PM
+    Author     : dibyendu_pc7
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hello</title>
+    </head>
+    <body>
+        <h1>${helloMessage}</h1>
+        </body>
+</html>
